41355b9697027dc62d9f7eb197f08bfc3d455df8
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-04-08 17:15:35 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,209 @@
+# 1-m
+roy = [1, 2, 3, 4, 5]
+print(roy)
+
+roy1 = list(map(lambda x: x * 2, roy))
+print(roy1)
+
+# 2-m
+roy = [10, 20, 30, 40]
+print(roy)
+
+roy1 = list(map(lambda x : x - 5, roy))
+print(roy1)
+
+# 3-m
+roy = ["apple", "banana", "cherry"]
+print(roy)
+
+roy1 = list(map(lambda x: x.upper(), roy))
+print(roy1)
+
+# 4-m
+roy = ["salom", "dunyo", "python"]
+print(roy)
+
+roy1 = list(map(lambda x: len(x),roy))
+print(roy1)
+
+# 5-m
+roy = [3, 6, 9, 12]
+print(roy)
+
+roy1 = list(map(lambda x: x ** 2, roy))
+print(roy1)
+
+# 6-m
+roy = ["Ali", "Vali", "Hasan"]
+print(roy)
+
+roy1 = list(map(lambda x: x[0], "Mr", roy))
+print(roy1)
+
+# 7-m
+roy = [100, 200, 300]
+print(roy)
+
+roy1 = list(map(lambda x: str(x), roy))
+print(roy1)
+
+# 8-m
+roy = ["1", "2", "3", "4"]
+print(roy)
+
+roy1 = list(map(lambda x :int(x), roy))
+print(roy1)
+
+# 9-m
+roy = [5, 10, 15, 20]
+print(roy)
+
+roy2 = list(map(lambda x: x / 3, roy))
+print(roy2)
+
+# 10-m
+y = ["hello", "world"]
+print(y)
+
+roy = list(map(lambda x: x + '!', y))
+print(roy)
+
+# 11-m
+roy = [2, 4, 6, 8]
+print(roy)
+
+roy1 = list(map(lambda x: x + 1, roy))
+print(roy1)
+
+# 12-m
+roy = ["a", "b", "c"]
+print(roy)
+
+roy1 = list(map(lambda x: x * 3, roy))
+print(roy1)
+
+# 13-m
+roy = [7, 14, 21]
+print(roy)
+
+roy1 = list(map(lambda x: x / 7, roy))
+print(roy1)
+
+# 14-m
+roy = ["Python", "Java", "C++"]
+print(roy)
+
+roy1 = list(map(lambda x: x.lower(), roy))
+print(roy1)
+
+# 15-m
+roy = [1, 2, 3, 4]
+print(roy)
+
+roy1 = list(map(lambda x:  f"son: {x}" , roy))
+print(roy1)
+
+# 16-m
+roy = ["10", "20", "30"]
+print(roy)
+
+roy1 = list(map(lambda x: int(x) + 10, roy))
+print(roy1)
+
+# 17-m
+roy = [9, 16, 25]
+print(roy)
+
+y = list(map(lambda x: x ** 0.5, roy))
+print(y)
+
+# 18-m
+roy = ["olma", "anor", "uzum"]
+print(roy)
+
+p = list(map(lambda x: x[0], roy))
+print(p)
+
+# 19-m
+roy = [11, 22, 33, 44]
+print(roy)
+
+roy1 = list(map(lambda x: str(x), roy))
+print(roy1)
+
+# 20-m
+roy = ["python", "map", "function"]
+print(roy)
+
+roy1 = list(map(lambda x: len(x) * 2 , roy))
+print(roy1)
+
+# 21-m
+roy = [0, 1, 2, 3]
+print(roy)
+
+roy1 = list(map(lambda x: x + 10, roy))
+print(roy1)
+
+# 22-m
+roy = ["x", "y", "z"]
+print(roy)
+
+harf = list(map(lambda x: f"harf {x}", roy))
+print(harf)
+
+# 23-m
+roy = [50, 100, 150]
+print(roy)
+
+roy1 = list(map(lambda x: x / 2, roy))
+print(roy1)
+
+# 24-m
+roy = ["5", "6", "7"]
+print(roy)
+
+roy1 = list(map(lambda x: int(x) ** 2, roy))
+print(roy1)
+
+# 25-m
+roy = [8, 9, 10]
+print(roy)
+
+roy1 = list(map(lambda x: x ** 2, roy))
+print(roy1)
+
+# 26-m
+roy = ["hi", "hello"]
+print(roy)
+
+roy1 = list(map(lambda x: len(x) + 1, roy))
+print(roy1)
+
+# 27-m
+roy = [12, 24, 36]
+print(roy)
+
+roy1 = list(map(lambda x: x / 12, roy))
+print(roy1)
+
+# 28-m
+roy = ["A", "B", "C"]
+print(roy)
+
+roy3 = list(map(lambda x: x.lower(), roy))
+print(roy3)
+
+# 29-m
+roy = [3, 5, 7]
+print(roy)
+
+d = list(map(lambda x: str(x * 10 ), roy))
+print(d)
+
+# 30-m
+roy = ["1", "2", "3"]
+print(roy)
+
+roy4 = list(map(lambda x: int(x)  + 100,   roy))
+print(roy4)
